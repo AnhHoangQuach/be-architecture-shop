6845263d6f8fb7dd95e810ee815f5c75c6b5f1f9
AnhHoangQuach <hoanganh36.work@gmail.com>
2023-05-29 09:50:12 +0700

    feat: rsa and jwt

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
@@ -9,6 +10,12 @@ const app = express()
 app.use(morgan('dev'))
 app.use(helmet())
 app.use(compression())
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
 
 // init db
 require('./dbs/init.mongodb.js')
@@ -16,12 +23,7 @@ const { checkOverload } = require('./helpers/check.connect')
 checkOverload()
 
 // init routes
-app.get('/', (req, res, next) => {
-  return res.status(200).json({
-    message: 'Welcome Fantipjs!',
-  })
-})
-
+app.use('/', require('./routes'))
 // handling errors
 
 module.exports = app
